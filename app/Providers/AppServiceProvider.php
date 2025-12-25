7047aa405411667092782975515eafe563eaad68
mr240-cyber <syauqi2403@gmail.com>
2025-12-25 18:05:46 +0800

    Config: Set timezone ke WITA (Asia/Makassar) dan locale Indonesia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Set Carbon locale to Indonesian
+        Carbon::setLocale('id');
+
+        // Set default timezone to WITA
+        date_default_timezone_set('Asia/Makassar');
     }
 }
